7d352019501f8fe8bda3f9f2e514aa93ceace69e
LaBlanca Ponder <lablancaponder@gmail.com>
2018-05-25 18:16:40 -0600

    first commit

---
+++ Assets.js
@@ -0,0 +1,29 @@
+let sprites = {};
+
+let assetsStillLoading = 0;
+
+function assetsLoadingLoop(callback) {
+  if (assetsStillLoading) {
+    requestAnimationFrame(assetsLoadingLoop.bind(this, callback));
+  } else {
+    callback();
+  }
+}
+
+function loadAssets(callback) {
+  function loadSprites(fileName) {
+    assetsStillLoading++;
+
+    let spriteImage = new Image();
+    spriteImage.src = "./assets/sprites/" + fileName;
+
+    spriteImage.onload = function() {
+      assetsStillLoading--;
+    };
+    return spriteImage;
+  }
+  sprites.background = loadSprites("tablebackground.jpg");
+  sprites.stick = loadSprites("stick.png");
+
+  assetsLoadingLoop(callback);
+}
